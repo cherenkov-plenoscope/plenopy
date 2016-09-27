10af0e19f8daf6a7c6ed89d92f1a1347f6cfcb18
sebastian <sebmuell@phys.ethz.ch>
2016-09-27 16:25:01 +0200

    we use the old LixelRays instead

--- plenopy/Tomography/RampKernel.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def ramp_kernel(kx=1, ky=1, kz=2):  
-    kernel3D = np.zeros(shape=(kz*2+1,kx*2+1,ky*2+1), dtype='float64')
-    for z in range(kernel3D.shape[0]):
-        for x in range(kernel3D.shape[1]):
-            for y in range(kernel3D.shape[2]):
-                kernel3D[z,x,y] = np.abs(z-kz) + np.abs(x-kx) + np.abs(y-ky) 
-    kernel3D = kernel3D/kernel3D.sum()
