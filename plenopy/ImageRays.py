b6775caf18003edc9fc115de20fa89738b573f5d
sebastian <sebmuell@phys.ethz.ch>
2016-10-15 17:52:52 +0200

    need image rays for faster refocussing

---
+++ plenopy/ImageRays.py
@@ -0,0 +1,69 @@
+import numpy as np
+
+
+class ImageRays(object):
+    """
+    support     [number_lixel x 3]
+                Support vectors of all the lixel rays on the principal aperture
+                plane. [x_mean, y_mean, 0]
+
+    direction   [number_lixel x 3]
+                Direction vectors of all the image rays after passing the 
+                imaging system.
+    """
+
+    def __init__(self, light_field):
+
+        # All in principal aperture frame
+        x = light_field.x_mean.flatten()
+        y = light_field.y_mean.flatten()
+
+        cx = light_field.cx_mean.flatten()
+        cy = light_field.cy_mean.flatten()
+
+        self._f = light_field.expected_focal_length_of_imaging_system
+        bs = light_field.sensor_plane2imaging_system.light_filed_sensor_distance
+
+        number_lixel = x.shape[0]
+
+        # 3d intersection with image sensor plane
+        img = np.array([
+            bs*np.tan(cx), 
+            bs*np.tan(cy), 
+            bs*np.ones(number_lixel)]).T
+
+        self.support = np.array([x, y, np.zeros(number_lixel)]).T
+        
+        self.direction = img - self.support
+        no = np.linalg.norm(self.direction, axis=1)
+        self.direction[:,0] /= no
+        self.direction[:,1] /= no
+        self.direction[:,2] /= no
+
+
+    def cx_cy_in_object_distance(self, object_distance):
+        """
+        Returns the cx and cy pixel directions when refocussing to the desired
+        object_distance.
+
+        Parameters
+        ----------
+        object_distance     Objects in this distance are mapped sharp inthe image.
+        """
+
+        image_distance = 1/(1/self._f - 1/object_distance)
+
+        scale_factors = image_distance/self.direction[:, 2]
+        pos = self.support + (scale_factors * self.direction.T).T
+        ix = pos[:,0]
+        iy = pos[:,1]
+
+        cx = np.arctan(ix/image_distance)
+        cy = np.arctan(iy/image_distance)
+        return cx, cy
+
+    def __repr__(self):
+        out = 'ImageRays('
+        out += str(self.support.shape[0]) + ' image rays'
+        out += ')\n'
+        return out
--- plenopy/__init__.py
+++ plenopy/__init__.py
@@ -1,6 +1,7 @@
 from .RawLighFieldSensorResponse import RawLighFieldSensorResponse
 from .LixelStatistics import LixelStatistics
 from .LixelRays import LixelRays
+from .ImageRays import ImageRays
 from .LightField import LightField
 from .Event import Event
 from .Run import Run
