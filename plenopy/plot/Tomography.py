939e60fb8e7475e4f520d757acc4209210a0233b
sebastian <sebmuell@phys.ethz.ch>
2016-10-26 21:13:34 +0200

    plot 2 slices of a 3D reconstruction at the same time using 2 color channels

--- plenopy/plot/Tomography.py
+++ plenopy/plot/Tomography.py
@@ -14,7 +14,8 @@ def save_slice_stack(
     binning, 
     intensity_volume, 
     output_path, 
-    image_prefix='slice_'):
+    image_prefix='slice_',
+    intensity_volume_2=None):
 
     fig_size = FigSize(dpi=200)
     fig = plt.figure(
@@ -26,6 +27,14 @@ def save_slice_stack(
 
     intensity_max = intensity_volume.max()
     intensity_min = intensity_volume.min()
+
+    if intensity_volume_2 is not None:
+        intensity_max_2 = intensity_volume_2.max()
+        intensity_min_2 = intensity_volume_2.min()
+    else:
+        intensity_max_2 = None
+        intensity_min_2 = None
+
     for z_slice in range(binning.number_z_bins):
 
         fig.suptitle(event.simulation_truth.event.short_event_info())
@@ -36,7 +45,11 @@ def save_slice_stack(
             binning=binning, 
             intensity_volume=intensity_volume,
             intensity_min=intensity_min,
-            intensity_max=intensity_max)
+            intensity_max=intensity_max,
+            intensity_volume_2=intensity_volume_2,
+            intensity_min_2=intensity_min_2,
+            intensity_max_2=intensity_max_2,
+            )
 
         add2ax_object_distance_ruler(
             ax=ax_object_distance_ruler,
@@ -59,20 +72,58 @@ def add2ax_z_slice(
     binning, 
     intensity_volume,
     intensity_min=None, 
-    intensity_max=None):
-
+    intensity_max=None,
+    intensity_volume_2=None,
+    intensity_min_2=None, 
+    intensity_max_2=None):
+    
     xy_lim = binning.xy_bin_centers.max()
-    img = ax.imshow(
-        intensity_volume[:,:,z_slice], 
-        cmap='viridis', 
-        extent=[-xy_lim, xy_lim, -xy_lim, xy_lim], 
-        interpolation='None')
-    img.set_clim(intensity_min, intensity_max)
     ax.set_xlabel('x/m')
     ax.set_ylabel('y/m')
-    divider = make_axes_locatable(ax)
-    cax = divider.append_axes("right", size="5%", pad=0.05)
-    plt.colorbar(img, cax=cax)
+
+    if intensity_volume_2 is None:
+        img = ax.imshow(
+            intensity_volume[:,:,z_slice], 
+            cmap='viridis', 
+            extent=[-xy_lim, xy_lim, -xy_lim, xy_lim], 
+            interpolation='None')
+        img.set_clim(intensity_min, intensity_max)
+        divider = make_axes_locatable(ax)
+        cax = divider.append_axes("right", size="5%", pad=0.05)
+        plt.colorbar(img, cax=cax)
+    else:
+        image = matrix_2_rgb_image(
+            intensity_volume[:,:,z_slice], 
+            color_channel=2,
+            intensity_min=intensity_min,
+            intensity_max=intensity_max) + matrix_2_rgb_image(
+            intensity_volume_2[:,:,z_slice], 
+            color_channel=0,
+            intensity_min=intensity_min_2,
+            intensity_max=intensity_max_2) 
+        img = ax.imshow(
+            image, 
+            extent=[-xy_lim, xy_lim, -xy_lim, xy_lim], 
+            interpolation='None')
+
+
+def matrix_2_rgb_image(
+    matrix,
+    color_channel=0,
+    intensity_min=None, 
+    intensity_max=None):
+
+    if intensity_min is None:
+        intensity_min = matrix.min()
+
+    if intensity_max is None:
+        intensity_max = matrix.max()
+
+    image = np.zeros(shape=(matrix.shape[0], matrix.shape[1], 3))
+
+    inensity = (matrix - intensity_min)/(intensity_max - intensity_min)
+    image[:,:,color_channel] = inensity
+    return image
 
 
 def save_slice_video(
@@ -80,7 +131,8 @@ def save_slice_video(
     binning, 
     intensity_volume, 
     output_path,
-    fps=25):
+    fps=25,
+    intensity_volume_2=None):
 
     with tempfile.TemporaryDirectory() as work_dir:
         image_prefix = 'slice_'
@@ -89,7 +141,8 @@ def save_slice_video(
             binning=binning,
             intensity_volume=intensity_volume,
             output_path=work_dir,
-            image_prefix=image_prefix)
+            image_prefix=image_prefix,
+            intensity_volume_2=intensity_volume_2)
         steps=binning.number_z_bins
 
         # duplicate the images and use them again in reverse order
--- plenopy/tests/test_tomography.py
+++ plenopy/tests/test_tomography.py
@@ -20,3 +20,61 @@ def test_binning_flat_voxel_center_positions():
                 assert xyz_flat[i,1] == binning.xy_bin_centers[y]
                 assert xyz_flat[i,2] == binning.z_bin_centers[z]
                 i+=1
+
+
+def test_matrix_2_image_shape():
+    matrix2d = np.eye(3)
+    image_bgr = plp.plot.Tomography.matrix_2_rgb_image(matrix=matrix2d)
+
+    assert matrix2d.shape[0] == image_bgr.shape[0]
+    assert matrix2d.shape[1] == image_bgr.shape[1]
+    assert matrix2d.shape == (3,3)
+    assert image_bgr.shape == (3,3,3)
+
+
+def test_matrix_2_image_rectangular_shape():
+    matrix2d = np.ones(shape=(16,9))
+    image_bgr = plp.plot.Tomography.matrix_2_rgb_image(matrix=matrix2d)
+
+    assert matrix2d.shape[0] == image_bgr.shape[0]
+    assert matrix2d.shape[1] == image_bgr.shape[1]
+    assert matrix2d.shape == (16,9)
+    assert image_bgr.shape == (16,9,3)
+
+
+def test_matrix_2_image_color_channel():
+    for color_channel in range(3):
+        matrix2d = np.eye(3)
+        image_bgr = plp.plot.Tomography.matrix_2_rgb_image(
+            matrix=matrix2d,
+            color_channel=color_channel)
+
+        # again test shape
+        assert matrix2d.shape[0] == image_bgr.shape[0]
+        assert matrix2d.shape[1] == image_bgr.shape[1]
+        assert matrix2d.shape == (3,3)
+        assert image_bgr.shape == (3,3,3)
+
+        # here is the matrix content
+        assert image_bgr[0,0,color_channel] == 1.0
+        assert image_bgr[1,1,color_channel] == 1.0
+        assert image_bgr[2,2,color_channel] == 1.0
+
+        # here is the background
+        assert image_bgr[0,1,color_channel] == 0
+        assert image_bgr[0,2,color_channel] == 0
+        assert image_bgr[1,0,color_channel] == 0
+        assert image_bgr[1,2,color_channel] == 0
+        assert image_bgr[2,0,color_channel] == 0
+        assert image_bgr[2,1,color_channel] == 0
+
+def test_matrix_2_image_min_max():
+    color_channel = 1
+    matrix2d = np.diag([0,0, 0.365, 1.0])
+    image_bgr = plp.plot.Tomography.matrix_2_rgb_image(
+        matrix=matrix2d,
+        color_channel=color_channel)
+
+    assert matrix2d[0,0] == image_bgr[0,0,color_channel]
+    assert matrix2d[1,1] == image_bgr[1,1,color_channel]
+    assert matrix2d[2,2] == image_bgr[2,2,color_channel]
