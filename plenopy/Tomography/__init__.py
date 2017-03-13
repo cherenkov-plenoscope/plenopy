443716ed7b0b9bebbe2b1b3aa8b79fc75c9df028
sebastian <sebmuell@phys.ethz.ch>
2017-03-13 15:44:18 +0100

    A 'narrow amgle tomography' or 3D deconvolution based on 'Light Field Microscopy' by Marc Levoy, Ren Ng and friend.

--- plenopy/Tomography/__init__.py
+++ plenopy/Tomography/__init__.py
@@ -1,5 +1,6 @@
 from .Binning import Binning
 from .AirShowerReconstruction import AirShowerReconstruction
-from .histogram_photon_bunches import histogram_photon_bunches
 from .plot import plot
 from .plot import flatten
+from . import simulation_truth
+from . import narrow_angle
