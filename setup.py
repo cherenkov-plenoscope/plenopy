75940572175b9820c0a05ff2d50fbc805ebdef78
sebastian <sebmuell@phys.ethz.ch>
2017-04-06 19:01:15 +0200

    A new era, now using the single pulse format

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+
+import numpy
 
 setup(
     name='plenopy',
-    version='0.0.0',
+    version='0.1.0',
     description='View and work on plenoscope events',
     url='',
     author='Sebastian Mueller, Max L. Ahnen, Dominik Neise',
@@ -20,5 +24,11 @@ setup(
     entry_points={'console_scripts': [
         'mctPlenoscopePlotLixelStatistics = plenopy.main.plot_lixel_statistics:main',
     ]},
-    zip_safe=False
+    zip_safe=False,
+    cmdclass = {'build_ext': build_ext},
+    ext_modules = [Extension("plenopy.cython_tools",
+                            sources=["plenopy/cython_tools.pyx", "plenopy/photon_stream_reader.cpp"],
+                            include_dirs=[numpy.get_include(), "plenopy"],
+                            language="c++",
+                            extra_compile_args=['-std=c++0x'])],
 )
