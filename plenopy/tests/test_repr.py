185c094ee9068e76a2e9176ce78adbf636ca206e
sebastian <sebmuell@phys.ethz.ch>
2017-04-10 16:52:49 +0200

    more __repr__ tests of simulation truth

--- plenopy/tests/test_repr.py
+++ plenopy/tests/test_repr.py
@@ -23,3 +23,9 @@ def test_event():
         'tests/resources/run.acp')
     event = pl.Run(path)[0]
     print(event.__repr__())
+    print(event.light_field.__repr__())
+    print(event.sensor_plane2imaging_system.__repr__())
+    print(event.simulation_truth.__repr__())
+    print(event.simulation_truth.event.__repr__())
+    print(event.simulation_truth.air_shower_photon_bunches.__repr__())
+    print(event.simulation_truth.detector.__repr__())
