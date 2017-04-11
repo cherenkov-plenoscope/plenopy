3eaabf6b6b0d947099043ca723a9b0097cca9520
sebastian <sebmuell@phys.ethz.ch>
2017-04-11 11:22:44 +0200

    use always 'pl' to shorten the plenopy import

--- plenopy/tests/test_idealized_run_reading.py
+++ plenopy/tests/test_idealized_run_reading.py
@@ -1,6 +1,6 @@
 import pytest
 import numpy as np
-import plenopy as plp
+import plenopy as pl
 import pkg_resources
 
 
@@ -8,7 +8,7 @@ def test_open_idealied_run():
     run_path = pkg_resources.resource_filename(
         'plenopy', 
         'tests/resources/idealized_example_run.corsika')
-    run = plp.IdealizedPlenoscope.Run(run_path)
+    run = pl.IdealizedPlenoscope.Run(run_path)
     assert run.number_events == 5
     assert run_path in run.path
     assert np.array_equal(run.event_numbers, np.array([1,2,3,4,5]))
@@ -18,6 +18,6 @@ def test_open_idealized_event():
     run_path = pkg_resources.resource_filename(
         'plenopy', 
         'tests/resources/idealized_example_run.corsika')
-    run = plp.IdealizedPlenoscope.Run(run_path)
+    run = pl.IdealizedPlenoscope.Run(run_path)
     evt = run[0]
     assert evt.number == 1
