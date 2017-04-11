49de28a4d953c5ace304b8c0433e32cf0992fede
sebastian <sebmuell@phys.ethz.ch>
2017-04-11 19:30:49 +0200

    move also the flattening into the tomography plot tool collection

--- plenopy/plot/Tomography.py
+++ plenopy/plot/Tomography.py
@@ -220,4 +220,33 @@ def plot_flat(xyzIs, xyzIs2=None, alpha_max=1.0, ball_size=25, steps=32):
     if xyzIs2 is not None:
         add2ax_flat(ax, xyzIs2, color='r', steps=steps, alpha_max=alpha_max,ball_size=ball_size)
 
-    plt.show()
+
+def flatten(hist, binning, threshold=0):
+    """
+    Returns a flat array of x,y,z positions and intensities for each voxel in
+    the 3D histogram.
+
+    Parameters
+    ----------
+    hist            A 3D intensity histogram with the shape specified in the 
+                    binning parameter.
+
+    binning         The 3D volume binning used to create the 3D intensity 
+                    histogram.
+
+
+    threshold       Voxels in the 3D intensity histogram below this threshold 
+                    are neglected.
+    """
+    xyzi = []
+    for x in range(hist.shape[0]):
+        for y in range(hist.shape[1]):
+            for z in range(hist.shape[2]):
+                if hist[x,y,z] > threshold:
+                    xyzi.append(np.array([
+                        binning.xy_bin_centers[x],
+                        binning.xy_bin_centers[y],
+                        binning.z_bin_centers[z],
+                        hist[x,y,z]]))
+    xyzi = np.array(xyzi)
+    return xyzi
