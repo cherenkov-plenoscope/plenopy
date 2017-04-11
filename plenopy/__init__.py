6c6d47de4344cca88e03b3a25fe9d259653d09aa
sebastian <sebmuell@phys.ethz.ch>
2017-04-11 09:37:50 +0200

    have the same naming for the cpp and py versions of the photonstream reader

--- plenopy/__init__.py
+++ plenopy/__init__.py
@@ -13,4 +13,4 @@ from . import SimulationTruth
 from . import Tomography
 from . import tools
 from .trigger_study import *
-from .py_stream2sequence import *
+from .photon_stream_reader import py_stream2sequence
