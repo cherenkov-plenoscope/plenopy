69d52634a82ec18423a9849162b0cc3f4ff7a459
sebastian <sebmuell@phys.ethz.ch>
2017-05-04 19:10:13 +0200

    optional label and value printing

--- plenopy/tools/add2ax_object_distance_ruler.py
+++ plenopy/tools/add2ax_object_distance_ruler.py
@@ -2,11 +2,13 @@ def add2ax_object_distance_ruler(
 	ax, 
 	object_distance, 
 	object_distance_min=0.0, 
-	object_distance_max=10e3):
+	object_distance_max=10e3,
+    label='object distance/km',
+    print_value=True):
     ax.set_xlim([0, 1])
     ax.set_ylim([object_distance_min/1e3, object_distance_max/1e3])
     ax.yaxis.tick_left()
-    ax.set_ylabel('object distance/km')
+    ax.set_ylabel(label)
     ax.spines['right'].set_visible(False)
     ax.spines['top'].set_visible(False)
     ax.spines['bottom'].set_visible(False)
@@ -15,4 +17,5 @@ def add2ax_object_distance_ruler(
     	[0, .5], 
     	[object_distance / 1e3, object_distance / 1e3], 
     	linewidth=5.0)
-    ax.text(0.0, -1.0, format(object_distance / 1e3, '.2f') + r' km')
+    if print_value:
+        ax.text(0.0, -1.0, format(object_distance / 1e3, '.2f') + r' km')
