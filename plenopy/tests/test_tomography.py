41ff1cd451e5cbd7aa7f97be4a18e06f6aa3de13
sebastian <sebmuell@phys.ethz.ch>
2017-06-29 10:01:16 +0200

    make the xyzI plot handle more correct intensities instead of just following a linear space

--- plenopy/tests/test_tomography.py
+++ plenopy/tests/test_tomography.py
@@ -3,6 +3,22 @@ import numpy as np
 import plenopy as pl
 import pkg_resources
 
+def test_array_chnunking():
+    chunking = pl.tomography.plot._start_and_end_slices_for_1D_array_chunking
+
+    (s,e) = chunking(number_of_chunks=10, array_length=10)
+    assert len(s) == 10
+    assert len(e) == 10
+    for i in range(len(s)):
+        assert s[i] < e[i]
+
+    (s,e) = chunking(number_of_chunks=10, array_length=100)
+    assert len(s) == 10
+    assert len(e) == 10
+    for i in range(len(s)):
+        assert s[i] < e[i]
+
+
 def test_binning_flat_voxel_center_positions():
     binning = pl.tomography.Binning(
         z_min=0.0,
--- plenopy/tomography/plot.py
+++ plenopy/tomography/plot.py
@@ -177,17 +177,60 @@ def save_slice_video(
             frames_per_second=fps)
 
 
-def add2ax_flat(ax, xyzIs, color='b', alpha_max=1.0, steps=32, ball_size=50.0):
+def _start_and_end_slices_for_1D_array_chunking(
+    number_of_chunks, 
+    array_length
+):
+    assert array_length >= number_of_chunks
+    assert array_length >= 0
+    assert number_of_chunks >= 0
+
+    step_length = int(np.ceil(array_length/number_of_chunks))
+    if step_length < 1:
+        step_length = 1
+
+    starts = []
+    ends = []
+    for step in range(number_of_chunks):
+        start = step*step_length
+        end = start+step_length+1
+        if end > array_length+1:
+            end = array_length+1
+        if start > array_length:
+            break
+        starts.append(start)
+        ends.append(end)
+    
+    return (np.array(starts), np.array(ends))
+
+
+def add2ax_flat(ax, xyzIs, color='b', alpha_max=0.2, steps=32, ball_size=100.0):
     intensities = xyzIs[:,3]
     xyzIs_sorted = xyzIs[np.argsort(intensities)]
-    chunk_suize = int(np.ceil(xyzIs.shape[0]/steps))
+    length = xyzIs_sorted.shape[0]
 
-    for step, alpha in enumerate(np.linspace(0.0025, alpha_max, steps)):
+    if steps > length:
+        steps = length
 
-        start = step*chunk_suize
-        end = start+chunk_suize
-        if end >= xyzIs.shape[0]:
-            end = xyzIs.shape[0]-1
+    (starts, ends) = _start_and_end_slices_for_1D_array_chunking(
+        number_of_chunks=steps, 
+        array_length=length)
+    
+    mean_chunk_intensities = []
+    for i in range(steps):
+        start = starts[i]
+        end = ends[i]
+        mean_chunk_intensities.append(xyzIs_sorted[start:end,3].mean())
+    mean_chunk_intensities = np.array(mean_chunk_intensities)
+
+    max_chunk_intensities = mean_chunk_intensities.max()
+
+    for i in range(steps):
+        start = starts[i]
+        end = ends[i]
+        mean_I = mean_chunk_intensities[i]
+        max_I = max_chunk_intensities
+        relative_I = mean_I/max_I
 
         ax.scatter(
             xyzIs_sorted[start:end,0], 
@@ -195,13 +238,13 @@ def add2ax_flat(ax, xyzIs, color='b', alpha_max=1.0, steps=32, ball_size=50.0):
             xyzIs_sorted[start:end,2],
             s=ball_size,
             depthshade=False,
-            alpha=alpha,
+            alpha=relative_I*alpha_max,
             c=color,
             lw=0
         )           
 
 
-def plot_flat(xyzIs, xyzIs2=None, alpha_max=1.0, ball_size=25, steps=32):
+def plot_flat(xyzIs, xyzIs2=None, alpha_max=0.2, steps=32, ball_size=100.0):
     """
     Plot a 3D intensity distribution. Can plot two distributions at the same 
     time using blue and red color.
