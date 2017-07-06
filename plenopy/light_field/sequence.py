34acf6821a20224e1dc42aead5faffe04476dcca
sebastian <sebmuell@phys.ethz.ch>
2017-07-06 13:16:54 +0200

    spaces no tabs

--- plenopy/light_field/sequence.py
+++ plenopy/light_field/sequence.py
@@ -13,18 +13,18 @@ def integrate_around_arrival_peak(
     sequece, 
     integration_radius=1
 ):
-	'''
-	Reduces a sequence of light fields or images to just one light field or 
+    '''
+    Reduces a sequence of light fields or images to just one light field or 
     image integrated around the main arrival intensity.
 
-	Parameters
-	----------
+    Parameters
+    ----------
 
-	sequece 				Matrix 2D, sequence of e.g. light fields or images, 
-							where the time slices go along axis=0.
+    sequece                 Matrix 2D, sequence of e.g. light fields or images, 
+                            where the time slices go along axis=0.
 
-	integration_radius 		Integer, integration radius in units of time slices.
-	'''
+    integration_radius      Integer, integration radius in units of time slices.
+    '''
     peak_slice = time_slice_with_max_intensity(sequece)
     start_slice = np.max([peak_slice - integration_radius, 0])
     stop_slice = np.min([peak_slice + integration_radius + 1, sequece.shape[0]-1])
