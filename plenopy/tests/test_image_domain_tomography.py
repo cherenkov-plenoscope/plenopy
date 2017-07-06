6289e39844e9c006c2d0a12e5dcfbd857b4249d3
sebastian <sebmuell@phys.ethz.ch>
2017-07-06 22:37:26 +0200

    explicit setting of number bins

--- plenopy/tests/test_image_domain_tomography.py
+++ plenopy/tests/test_image_domain_tomography.py
@@ -5,18 +5,22 @@ import pkg_resources
 
 
 def test_default_image_domain_binning():
-    b = pl.tomography.image_domain.DepthOfFieldBinning()
-    assert b.x_img_num == 32
-    assert b.y_img_num == 32
-    assert b.b_img_num == 32
+    b = pl.tomography.image_domain.DepthOfFieldBinning(
+        cx_num=64,
+        cy_num=64,
+        obj_num=64,
+    )
+    assert b.x_img_num == 64
+    assert b.y_img_num == 64
+    assert b.b_img_num == 64
 
-    assert len(b.x_img_bin_edges) == 33
-    assert len(b.y_img_bin_edges) == 33
-    assert len(b.b_img_bin_edges) == 33
+    assert len(b.x_img_bin_edges) == 65
+    assert len(b.y_img_bin_edges) == 65
+    assert len(b.b_img_bin_edges) == 65
 
-    assert len(b.x_img_bin_centers) == 32
-    assert len(b.y_img_bin_centers) == 32
-    assert len(b.b_img_bin_centers) == 32
+    assert len(b.x_img_bin_centers) == 64
+    assert len(b.y_img_bin_centers) == 64
+    assert len(b.b_img_bin_centers) == 64
 
 
 def test_image_domain_binning_repr():
