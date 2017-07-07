5e884e0a85faf88ae660f84794427690f8b82f41
sebastian <sebmuell@phys.ethz.ch>
2017-07-07 23:13:28 +0200

    enable to pass only the photons that fall in a given instruments acceptance, aperture radius, fov radius

--- plenopy/tomography/simulation_truth.py
+++ plenopy/tomography/simulation_truth.py
@@ -1,7 +1,13 @@
 import numpy as np
 
 
-def histogram_photon_bunches(photon_bunches, binning, observation_level=5e3):
+def histogram_photon_bunches(
+    photon_bunches, 
+    binning, 
+    limited_aperture_radius=None,
+    limited_fov_radius=None,
+    observation_level=5e3
+):
     """
     parameter
     ---------
@@ -17,21 +23,32 @@ def histogram_photon_bunches(photon_bunches, binning, observation_level=5e3):
                     the voxel intensity encodes the number count of photon 
                     production positions in this voxel.
     """
-    emission_positions = emission_positions_of_photon_bunches(
+    ep = emission_positions_of_photon_bunches(
         photon_bunches=photon_bunches, 
-        observation_level=observation_level)
+        observation_level=observation_level,
+        limited_aperture_radius=limited_aperture_radius,
+        limited_fov_radius=limited_fov_radius,
+    )
 
     hist = np.histogramdd(
-        emission_positions, bins=(
+        ep['emission_positions'][ep['valid_acceptence']], 
+        bins=(
             binning.xy_bin_edges, 
             binning.xy_bin_edges, 
-            binning.z_bin_edges),
-            weights=photon_bunches.probability_to_reach_observation_level)
+            binning.z_bin_edges
+            ),
+            weights=photon_bunches.probability_to_reach_observation_level[ep['valid_acceptence']]
+        )
 
     return hist[0]
 
 
-def emission_positions_of_photon_bunches(photon_bunches, observation_level=5e3):
+def emission_positions_of_photon_bunches(
+    photon_bunches, 
+    limited_aperture_radius=None,
+    limited_fov_radius=None,
+    observation_level=5e3,
+):
     """
     parameter
     ---------
@@ -66,4 +83,22 @@ def emission_positions_of_photon_bunches(photon_bunches, observation_level=5e3):
     # transform to plenoscope frame
     emission_positions[:,2] = emission_positions[:,2] - observation_level
 
-    return emission_positions
+
+    valid_aperture = np.ones(supports.shape[0], dtype=np.bool)
+    valid_fov = np.ones(supports.shape[0], dtype=np.bool)
+
+    if limited_aperture_radius is not None:
+        R_aperture = np.sqrt(supports[:,0]**2 + supports[:,1]**2)
+        valid_aperture = R_aperture <= limited_aperture_radius
+
+    if limited_fov_radius is not None:
+        R_fov = np.sqrt(directions[:,0]**2 + directions[:,1]**2)
+        valid_fov = R_fov <= limited_fov_radius
+
+    valid_acceptence = valid_aperture*valid_fov
+    print('survived', valid_acceptence.sum()/valid_acceptence.shape[0])
+
+    return {
+        'emission_positions': emission_positions,
+        'valid_acceptence': valid_acceptence
+    }
