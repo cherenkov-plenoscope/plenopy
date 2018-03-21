b2ba4aff4579afc976cd74eeda5ca660ce698d91
sebastian <sebmuell@phys.ethz.ch>
2018-03-21 20:32:30 +0100

    A 2nd trigger taking advantage of the light-field refocussing and truncatiuon of low p.e. instead of masking.

--- plenopy/trigger.py
+++ plenopy/trigger.py
@@ -1,5 +1,6 @@
 import numpy as np
 from .light_field.sequence import integrate_around_arrival_peak
+from .image import ImageRays
 import scipy.spatial.distance
 
 
@@ -47,7 +48,6 @@ def trigger_1(
 
 def make_trigger_image_based_on_light_field(
     light_field_pixel_paxel,
-    time_integration_radius_in_slices=2,
     min_photons_in_lixel=2,
     min_paxels_above_threshold_in_pixel=3,
 ):
@@ -70,6 +70,92 @@ def neighborhood(x, y, epsilon):
         np.vstack([x, y]).T)
     nn = (dists <= epsilon) * (dists != 0)
     return nn
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def trigger_windows(
+    light_field_sequence,
+    trigger_integration_time_window_in_slices=5,
+):
+    trigger_window = trigger_integration_time_window_in_slices
+    num_time_slices = light_field_sequence.shape[0]
+    num_trigger_trials = num_time_slices - trigger_window
+    trigger_windows = np.zeros(
+        shape=(num_trigger_trials, light_field_sequence.shape[1]))
+    for s in range(num_trigger_trials):
+        trigger_windows[s, :] = light_field_sequence[s:s+trigger_window, :].sum(axis=0)
+    return trigger_windows
+
+
+def prepare_trigger_3(
+    light_field_geometry,
+    object_disatances=[5e3, 10e3, 25e3, 20e3, 25e3]
+):
+    lfg = light_field_geometry
+    num_refocuses = len(object_disatances)
+    image_rays = ImageRays(lfg)
+    image_fov = lfg.sensor_plane2imaging_system.max_FoV_diameter
+    pixel_fov = lfg.sensor_plane2imaging_system.pixel_FoV_hex_flat2flat
+    num_pixel_1D = int(np.ceil(image_fov/pixel_fov))
+    pixel_edges = np.linspace(-image_fov/2, image_fov/2, num_pixel_1D + 1)
+
+    refocus_cx = np.zeros(shape=(num_refocuses, lfg.number_lixel))
+    refocus_cy = np.zeros(shape=(num_refocuses, lfg.number_lixel))
+
+    for i, obj in enumerate(object_disatances):
+        cx, cy = image_rays.cx_cy_in_object_distance(obj)
+        refocus_cx[i, :] = cx
+        refocus_cy[i, :] = cy
+
+    return {
+        'refocus_cx': refocus_cx,
+        'refocus_cy': refocus_cy,
+        'pixel_edges': pixel_edges
+    }
+
+
+def trigger_3(
+    light_field,
+    refocus_cx,
+    refocus_cy,
+    pixel_edges,
+    min_photons_in_lixel=2,
+):
+    num_refocuses = refocus_cx.shape[0]
+    num_pixel_1D = pixel_edges.shape[0] - 1
+    refocused_images = np.zeros(
+        shape=(
+            num_refocuses,
+            num_pixel_1D,
+            num_pixel_1D))
+
+    truncated_light_field = light_field.copy()
+    truncated_light_field[light_field < min_photons_in_lixel] = 0
+
+    for i in range(num_refocuses):
+        refocused_images[i, :, :] = np.histogram2d(
+            refocus_cx[i, :],
+            refocus_cy[i, :],
+            weights=truncated_light_field,
+            bins=[pixel_edges, pixel_edges])[0]
+
+    max_pixel_for_object_distance = refocused_images.max(axis=1).max(axis=1)
+
+    return max_pixel_for_object_distance, refocused_images
 
 
 
