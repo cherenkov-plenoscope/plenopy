72d2bdf925bc402f1c7e54ab7901eb6099c723ab
sebastian <sebmuell@phys.ethz.ch>
2018-04-09 14:01:21 +0200

    convert to float32 arrays internally

--- plenopy/photon_stream/cython_reader.pyx
+++ plenopy/photon_stream/cython_reader.pyx
@@ -60,17 +60,14 @@ def stream2_cx_cy_arrivaltime_point_cloud(
     np.ndarray[unsigned char, ndim=1, mode="c"] photon_stream not None,
     float time_slice_duration,
     unsigned char NEXT_READOUT_CHANNEL_MARKER,
-    np.ndarray[float, ndim=1, mode="c"] cx not None,
-    np.ndarray[float, ndim=1, mode="c"] cy not None,
-    np.ndarray[float, ndim=1, mode="c"] time_delay not None):
+    cx,
+    cy,
+    time_delay):
 
     cdef unsigned int photon_stream_length
     photon_stream_length = photon_stream.shape[0]
 
     number_lixel = 1 + np.sum(photon_stream == NEXT_READOUT_CHANNEL_MARKER)
-    assert len(cx) == number_lixel, 'There must be a cx for each lixel.'
-    assert len(cy) == number_lixel, 'There must be a cy for each lixel.'
-    assert len(time_delay) == number_lixel, 'There must be a time_delay for each lixel.'
 
     number_photons = (photon_stream.shape[0] - (number_lixel - 1))
     cdef np.ndarray[float, mode = "c"] point_cloud = np.ascontiguousarray(
@@ -81,14 +78,32 @@ def stream2_cx_cy_arrivaltime_point_cloud(
         np.zeros(number_photons, dtype=np.uint32),
         dtype=np.uint32)
 
+    assert cx is not None
+    assert len(cx) == number_lixel, 'There must be a cx for each lixel.'
+    cdef np.ndarray[float, mode = "c"] _cx = np.ascontiguousarray(
+        cx,
+        dtype=np.float32)
+
+    assert cy is not None
+    assert len(cy) == number_lixel, 'There must be a cy for each lixel.'
+    cdef np.ndarray[float, mode = "c"] _cy = np.ascontiguousarray(
+        cy,
+        dtype=np.float32)
+
+    assert time_delay is not None
+    assert len(time_delay) == number_lixel, 'There must be a time_delay for each lixel.'
+    cdef np.ndarray[float, mode = "c"] _time_delay = np.ascontiguousarray(
+        time_delay,
+        dtype=np.float32)
+
     c_stream2_cx_cy_arrivaltime_point_cloud(
         &photon_stream[0],
         photon_stream_length,
         NEXT_READOUT_CHANNEL_MARKER,
         &point_cloud[0],
-        &cx[0],
-        &cy[0],
-        &time_delay[0],
+        &_cx[0],
+        &_cy[0],
+        &_time_delay[0],
         time_slice_duration,
         &lixel_ids[0])
 
