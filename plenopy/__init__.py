e06c35a0f5c807aad756c512e1c78dc71b1fc11c
sebastian <sebmuell@phys.ethz.ch>
2018-04-15 13:38:50 +0200

    drop the idealized plenoscope, rmfrom __init__.py

--- plenopy/__init__.py
+++ plenopy/__init__.py
@@ -4,7 +4,6 @@ from .event.Event import Event
 from .Run import Run
 from .image.Image import Image
 from . import plot
-from . import idealized_plenoscope
 from . import corsika
 from . import simulation_truth
 from . import tomography
