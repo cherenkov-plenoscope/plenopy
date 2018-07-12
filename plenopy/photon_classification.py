7a73c5f4f1df375c04df39f92fa383b8e6ede637
sebastian <sebmuell@phys.ethz.ch>
2018-07-12 11:57:31 +0200

    a simpler classifier and a benchmark tool

--- plenopy/photon_classification.py
+++ plenopy/photon_classification.py
@@ -7,94 +7,6 @@ from .image.ImageRays import ImageRays
 from . import trigger
 
 
-def classify_air_shower_photons(
-    light_field_geometry,
-    raw_sensor_response,
-    start_time_roi,
-    end_time_roi,
-    cx_center_roi,
-    cy_center_roi,
-    cx_cy_radius_roi,
-    object_distances,
-    deg_over_s,
-    epsilon_cx_cy_radius,
-    min_number_photons,
-):
-    """
-    Classifiy air-shower-photons and night-sky-background photons based on the
-    higher density of air-shower-photons in the refocussed image-sequences.
-
-    To speed up the classification, you have to restrict to a Region of Interest
-    (roi) in both directions cx, and cy, and time.
-    """
-    imrays = ImageRays(light_field_geometry)
-    air_shower_photon_ids = []
-    for i, object_distance in enumerate(object_distances):
-        cx, cy = imrays.cx_cy_in_object_distance(object_distance)
-        cxcyt, lixel_ids = stream2_cx_cy_arrivaltime_point_cloud(
-            photon_stream=raw_sensor_response.photon_stream,
-            time_slice_duration=raw_sensor_response.time_slice_duration,
-            NEXT_READOUT_CHANNEL_MARKER=raw_sensor_response.NEXT_READOUT_CHANNEL_MARKER,
-            cx=cx,
-            cy=cy,
-            time_delay=light_field_geometry.time_delay_mean)
-
-        photon_ids = np.arange(
-            raw_sensor_response.number_photons,
-            dtype=np.uint32)
-
-        mask_time_roi = mask_time_in_point_cloud(
-            pcl=cxcyt,
-            start_time=start_time_roi,
-            end_time=end_time_roi)
-
-        photon_ids_t = photon_ids[mask_time_roi]
-        lixel_ids_t = lixel_ids[mask_time_roi]
-        cxcyt_t = cxcyt[mask_time_roi]
-
-        mask_cx_cy_roi = mask_cxcy_in_point_cloud(
-            cx=cxcyt_t[:, 0],
-            cy=cxcyt_t[:, 1],
-            cx_center=cx_center_roi,
-            cy_center=cy_center_roi,
-            c_radius=cx_cy_radius_roi)
-
-        photon_ids_tc = photon_ids_t[mask_cx_cy_roi]
-        lixel_ids_tc = lixel_ids_t[mask_cx_cy_roi]
-        cxcyt_tc = cxcyt_t[mask_cx_cy_roi]
-
-        photon_labels = cluster_air_shower_photons_based_on_density(
-            cx_cy_arrival_time_point_cloud=cxcyt_tc,
-            epsilon_cx_cy_radius=epsilon_cx_cy_radius,
-            min_number_photons=min_number_photons,
-            deg_over_s=deg_over_s)
-        mask_air_shower = photon_labels >= 0
-
-        photon_ids_air_shower = photon_ids_tc[mask_air_shower]
-        cxcyt_air_shower = cxcyt_tc[mask_air_shower]
-        lixel_ids_air_shower = lixel_ids_tc[mask_air_shower]
-
-        air_shower_photon_ids.append(photon_ids_air_shower)
-
-    w = []
-    for l in air_shower_photon_ids:
-        for s in l:
-            w.append(s)
-    air_shower_photon_ids = np.array(list(set(w)))
-    return air_shower_photon_ids, lixel_ids
-
-
-def mask_time_in_point_cloud(pcl, start_time, end_time):
-    before_end = pcl[:, 2] <= end_time
-    after_start = pcl[:, 2] > start_time
-    return before_end*after_start
-
-
-def mask_cxcy_in_point_cloud(cx, cy, cx_center, cy_center, c_radius):
-    distance_to_center_2 = (cx - cx_center)**2 + (cy - cy_center)**2
-    return distance_to_center_2 <= c_radius**2
-
-
 def cluster_air_shower_photons_based_on_density(
     cx_cy_arrival_time_point_cloud,
     epsilon_cx_cy_radius=np.deg2rad(0.1),
@@ -112,41 +24,6 @@ def cluster_air_shower_photons_based_on_density(
     ).fit(xyt)
 
     return dbscan.labels_
-
-
-def classify_air_shower_photons_from_trigger_response(
-    event,
-    trigger_region_of_interest,
-    roi_time_radius=5e-9,
-    roi_cx_cy_radius=np.deg2rad(0.3),
-    roi_object_distance_radius=5e3,
-    deg_over_s=0.20e9,
-    refocusses_for_classification=7,
-    epsilon_cx_cy_radius=np.deg2rad(0.055),
-    min_number_photons=9
-):
-    """
-    These defaults give for the 71m ACP 61-paxel x 8433-pixel:
-    true air-shower-photons over nsb-photons: 0.62
-    median number of photons in cluster: 131
-    fratction of all true air-shower-photons: 0.65
-    """
-    roi = trigger_region_of_interest
-    return classify_air_shower_photons(
-        light_field_geometry=event.light_field_geometry,
-        raw_sensor_response=event.raw_sensor_response,
-        start_time_roi=roi['time_center_roi'] - roi_time_radius,
-        end_time_roi=roi['time_center_roi'] + roi_time_radius,
-        cx_center_roi=roi['cx_center_roi'],
-        cy_center_roi=roi['cy_center_roi'],
-        cx_cy_radius_roi=roi_cx_cy_radius,
-        object_distances=np.logspace(
-            np.log10(roi['object_distance'] - roi_object_distance_radius),
-            np.log10(roi['object_distance'] + roi_object_distance_radius),
-            refocusses_for_classification),
-        deg_over_s=deg_over_s,
-        epsilon_cx_cy_radius=epsilon_cx_cy_radius,
-        min_number_photons=min_number_photons)
 
 
 def center_for_region_of_interest(event):
@@ -228,3 +105,78 @@ class RawPhotons():
             t_pap=self.t_pap[mask],
             t_img=self.t_img[mask])
 
+
+def benchmark(
+    pulse_origins,
+    photon_ids_cherenkov
+):
+    """
+    Parameters
+    ----------
+    pulse_origins           Array 1D. The origins of the pulses in the raw
+                            response of the light-field-sensor. Positive
+                            integers encode the ids of the photon from the
+                            input file used in the mctracer photon-propagator.
+                            The input files are usually Cherenkov-photons from
+                            the KIT-CORSIKA air-shower-simulation.
+                            Negative integers encode night-sky-background and
+                            artifacts of the sensors.
+
+    photon_ids_cherenkov    Array 1D. Ids of the photons which got classified
+                            to be Cherenkov-photons from the air-shower.
+
+    Returns
+    -------
+    number of matches       The number of true positive, false positive, true
+                            negative, and false negative matches.
+
+    """
+    photon_ids_nsb = np.setdiff1d(
+        np.arange(pulse_origins.shape[0]),
+        photon_ids_cherenkov)
+
+    is_cherenkov = pulse_origins >= 0
+    is_nsb = pulse_origins < 0
+
+    return {
+        # is Cherenkov AND classified as Cherenkov
+        'number_true_positives': is_cherenkov[photon_ids_cherenkov].sum(),
+        # is Cherenkov AND classified as NSB
+        'number_false_positives': is_cherenkov[photon_ids_nsb].sum(),
+        # is NSB AND classified as Cherenkov
+        'number_false_negatives': is_nsb[photon_ids_cherenkov].sum(),
+        # is NSB AND classified as NSB
+        'number_true_negatives': is_nsb[photon_ids_nsb].sum()}
+
+
+def classify_cherenkov_photons_in_roi_in_image(
+    roi,
+    photons,
+    time_radius_roi=5e-9,
+    c_radius=np.deg2rad(0.3),
+    epsilon_cx_cy_radius=np.deg2rad(0.075),
+    min_number_photons=20,
+    deg_over_s=0.375e9,
+):
+    start_time = roi['time_center_roi'] - time_radius_roi
+    end_time = roi['time_center_roi'] + time_radius_roi
+    roi_mask_time = (
+        (photons.t_img >= start_time) & (photons.t_img < end_time))
+    ph_cx, ph_cy = photons.cx_cy_in_object_distance(roi['object_distance'])
+    ph_c_distance_square = (
+        (roi['cx_center_roi'] - ph_cx)**2 +
+        (roi['cy_center_roi'] - ph_cy)**2)
+    c_radius_square = c_radius**2
+    roi_mask_c = ph_c_distance_square <= c_radius_square
+    roi_mask = roi_mask_time & roi_mask_c
+    photons_in_roi = photons.cut(roi_mask)
+    photon_labels = cluster_air_shower_photons_based_on_density(
+        cx_cy_arrival_time_point_cloud=np.c_[
+            photons_in_roi.cx_cy_in_object_distance(roi['object_distance'])[0],
+            photons_in_roi.cx_cy_in_object_distance(roi['object_distance'])[1],
+            photons_in_roi.t_img],
+        epsilon_cx_cy_radius=epsilon_cx_cy_radius,
+        min_number_photons=min_number_photons,
+        deg_over_s=deg_over_s)
+    cherenkov_mask = photon_labels >= 0
+    return photons_in_roi.cut(cherenkov_mask)
---
+++ plenopy/tests/test_photon_classification.py
@@ -0,0 +1,63 @@
+import numpy as np
+import plenopy as pl
+
+
+def test_benchmark_of_perfect_detector():
+    number_all_photons = 1000*1000
+    number_cherenkov_photons = 100
+    pulse_origins = -100*np.ones(number_all_photons, dtype=np.int)
+    pulse_origins[0: number_cherenkov_photons] = np.arange(
+        number_cherenkov_photons)
+
+    photon_ids_cherenkov = np.arange(number_cherenkov_photons)
+
+    result = pl.photon_classification.benchmark(
+        pulse_origins=pulse_origins,
+        photon_ids_cherenkov=photon_ids_cherenkov)
+
+    assert (
+        result['number_true_positives'] == number_cherenkov_photons)
+    assert (
+        result['number_true_negatives'] ==
+        number_all_photons - number_cherenkov_photons)
+    assert result['number_false_negatives'] == 0
+    assert result['number_false_positives'] == 0
+    assert (
+            number_all_photons -
+            result['number_true_negatives'] -
+            result['number_true_positives'] -
+            result['number_false_positives'] -
+            result['number_false_negatives']
+        ) == 0
+
+
+def test_benchmark_of_normal_detector():
+    number_all_photons = 1000*1000
+    number_cherenkov_photons = 100
+    pulse_origins = -100*np.ones(number_all_photons, dtype=np.int)
+    pulse_origins[0: number_cherenkov_photons] = np.arange(
+        number_cherenkov_photons)
+
+    photon_ids_cherenkov = np.arange(
+        int(number_cherenkov_photons/2),
+        int(number_cherenkov_photons*1.5),
+        dtype=np.int)
+
+    result = pl.photon_classification.benchmark(
+        pulse_origins=pulse_origins,
+        photon_ids_cherenkov=photon_ids_cherenkov)
+
+    assert (
+        result['number_true_positives'] == number_cherenkov_photons/2)
+    assert (
+        result['number_true_negatives'] ==
+        number_all_photons - number_cherenkov_photons*1.5)
+    assert result['number_false_negatives'] == number_cherenkov_photons/2
+    assert result['number_false_positives'] == number_cherenkov_photons/2
+    assert (
+            number_all_photons -
+            result['number_true_negatives'] -
+            result['number_true_positives'] -
+            result['number_false_positives'] -
+            result['number_false_negatives']
+        ) == 0
