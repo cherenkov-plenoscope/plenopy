38d848cc99ba8220599ac10914e44da35cf3dfa7
Sebastian Achim Mueller <sebmuell@phys.ethz.ch>
2019-10-08 14:29:38 +0200

    mv out of plenopy

--- plenopy/tomography/__init__.py
+++ plenopy/tomography/__init__.py
@@ -3,6 +3,5 @@ from .Rays import Rays
 from . import filtered_back_projection
 from . import simulation_truth
 from . import narrow_angle
-from . import ray_and_voxel
 from . import image_domain
 from .baselines_in_voxels import baselines_in_voxels
