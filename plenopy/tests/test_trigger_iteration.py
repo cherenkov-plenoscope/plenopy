94c872b60203fa129ce0591f3ea66bcc8d3c2377
Sebastian Achim Mueller <sebmuell@phys.ethz.ch>
2019-10-08 14:43:08 +0200

    there is only the binary-search implementation left now.,

--- plenopy/tests/test_trigger_iteration.py
+++ plenopy/tests/test_trigger_iteration.py
@@ -14,27 +14,11 @@ def test_trigger_iteration():
         light_field_geometry=run.light_field_geometry,
         object_distances=[7.5e3, 15e3, 22.5e3])
 
-    trigger_response_linear = []
     trigger_response_binary = []
     for n, event in enumerate(run):
-        trigger_response = pl.trigger.__apply_refocus_sum_trigger(
-            event=event,
-            trigger_preparation=trigger_preparation,
-            min_number_neighbors=3,
-            integration_time_in_slices=5,
-            max_iterations=10000,)
-        trigger_response_linear.append(trigger_response)
-
         trigger_response = pl.trigger.apply_refocus_sum_trigger(
             event=event,
             trigger_preparation=trigger_preparation,
             min_number_neighbors=3,
             integration_time_in_slices=5,)
         trigger_response_binary.append(trigger_response)
-
-    for t in range(len(trigger_response_linear)):
-        for obj in range(3):
-            trb = trigger_response_binary[t][obj]
-            trl = trigger_response_linear[t][obj]
-            assert trl['iteration_converged'] == True
-            assert trl['patch_threshold'] == trb['patch_threshold']
