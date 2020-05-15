37bf148857644d31a7cc4075cc0e8c9c46ff7725
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2020-05-15 14:43:31 +0200

    mv hexagona grid to tools

--- plenopy/tools/__init__.py
+++ plenopy/tools/__init__.py
@@ -1,3 +1,4 @@
 from . import acp_format
 from . import ransac_3d_plane
 from . import header273float32
+from . import hexagonal_grid
---
+++ plenopy/tools/hexagonal_grid.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+
+def make_hexagonal_grid(
+    outer_radius,
+    spacing,
+    inner_radius=0.0
+):
+    hex_a = np.array([np.sqrt(3)/2, 0.5])*spacing
+    hex_b = np.array([0, 1])*spacing
+
+    grid = []
+    sample_radius = 2.0*np.floor(outer_radius/spacing);
+    for a in np.arange(-sample_radius, sample_radius+1):
+        for b in np.arange(-sample_radius, sample_radius+1):
+            cell_ab = hex_a*a + hex_b*b
+            cell_norm = np.linalg.norm(cell_ab)
+            if cell_norm <= outer_radius and cell_norm >= inner_radius:
+                grid.append(cell_ab);
+    return np.array(grid)
