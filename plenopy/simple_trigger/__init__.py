235ac283e37f762ca4d17634273764b69ce3f3a0
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2021-06-16 12:59:39 +0200

    readme for trigger

--- plenopy/simple_trigger/__init__.py
+++ plenopy/simple_trigger/__init__.py
@@ -1,3 +1,18 @@
+"""
+A simple sum-trigger
+--------------------
+
+Individual read-out-channels (light-field-cells) are added up and
+compared against a threshold.
+
+Here we sum the light-field-cells to create images refocused to certain
+object-distances.
+
+The total trigger-system can have multiple of these refocused images.
+
+The pattern of light-field-cells to be added up is stored in the
+trigger-geometry, which is estimated from the ligth-field-geometry.
+"""
 from . import io
 from . import prepare
 from . import estimate
